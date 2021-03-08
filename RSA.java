fecb1cd46dad641aeafac4ef3d89528d9f505bc3
ckodonnell <codonnell@vassar.edu>
2021-03-08 16:59:56 -0500

    started RSA method

---
+++ RSA.java
@@ -0,0 +1,17 @@
+import java.security.*;
+import java.util.*;
+
+public class RSA {
+    public static void main (String [] args) throws NoSuchAlgorithmException{
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        SecureRandom secureRandom = new SecureRandom();
+        keyGen.initialize(1024, secureRandom);
+    }
+    public byte [] encrypt (byte [] plainText){
+        return plainText;
+    }
+    public String decrypt (byte [] encryptedMessage){
+        String decryptedMessage = ""; 
+        return decryptedMessage;
+    }
+}
