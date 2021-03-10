c7f0626c8a224f484d60b6727ead95b81b3bf973
agbuller <abuller@vassar.edu>
2021-03-10 09:30:52 -0500

    Fixed encrypt method so that it works

--- RSA.java
+++ RSA.java
@@ -3,7 +3,7 @@ import java.util.*;
 import javax.crypto.*;
 
 public class RSA {
-    public static void main (String [] args) throws NoSuchAlgorithmException{
+    public static void main (String [] args) throws BadPaddingException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException{
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         SecureRandom secureRandom = new SecureRandom();
         keyGen.initialize(1024, secureRandom);
@@ -12,7 +12,7 @@ public class RSA {
         PrivateKey privateKey = pair.getPrivate();
         PublicKey publicKey = pair.getPublic();
 
-        static String originalText = "This is a secret message: RSA edition :)";
+         String originalText = "This is a secret message: RSA edition :)";
 
         System.out.println("Original Text : "+originalText);
 
@@ -20,19 +20,19 @@ public class RSA {
 
         System.out.println("Encrypted Text : "+Base64.getEncoder().encodeToString(encryptedText));
 
-        String decryptedText = decrypt(encryptedText,privatKey);
+        String decryptedText = decrypt(encryptedText,privateKey);
         System.out.println("Decrypted Text : "+decryptedText);
 
     }
-    public byte [] encrypt (String originalText, PublicKey public){
+    public static byte [] encrypt (String originalText, PublicKey pubKey) throws BadPaddingException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException{
 
-      Cipher cipher = Cipher.getInstance("RSA");
+      Cipher encrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
-      cipher.init(Cipher.ENCRYPT_MODE, public)
+      encrypt.init(Cipher.ENCRYPT_MODE, pubKey);
 
-        return cipher.doFinal(plainText.getBytes());
+        return encrypt.doFinal(originalText.getBytes());
     }
-    public String decrypt (byte [] encryptedMessage, PrivateKey private){
+    public static String decrypt (byte [] encryptedMessage, PrivateKey privKey){
         String decryptedMessage = "";
         return decryptedMessage;
     }
