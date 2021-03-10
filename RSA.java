c1507b6fa592e7aaa95d6407f0c8605eab88efb5
agbuller <abuller@vassar.edu>
2021-03-09 20:38:58 -0500

    added encrypt method

--- RSA.java
+++ RSA.java
@@ -1,5 +1,6 @@
 import java.security.*;
 import java.util.*;
+import javax.crypto.*;
 
 public class RSA {
     public static void main (String [] args) throws NoSuchAlgorithmException{
@@ -8,13 +9,31 @@ public class RSA {
         keyGen.initialize(1024, secureRandom);
 
         KeyPair pair = keyGen.generateKeyPair();
+        PrivateKey privateKey = pair.getPrivate();
+        PublicKey publicKey = pair.getPublic();
+
+        static String originalText = "This is a secret message: RSA edition :)";
+
+        System.out.println("Original Text : "+originalText);
+
+        byte[] encryptedText = encrypt(originalText, publicKey);
+
+        System.out.println("Encrypted Text : "+Base64.getEncoder().encodeToString(encryptedText));
+
+        String decryptedText = decrypt(encryptedText,privatKey);
+        System.out.println("Decrypted Text : "+decryptedText);
 
     }
-    public byte [] encrypt (byte [] plainText){
-        return plainText;
+    public byte [] encrypt (String originalText, PublicKey public){
+
+      Cipher cipher = Cipher.getInstance("RSA");
+
+      cipher.init(Cipher.ENCRYPT_MODE, public)
+
+        return cipher.doFinal(plainText.getBytes());
     }
-    public String decrypt (byte [] encryptedMessage){
-        String decryptedMessage = ""; 
+    public String decrypt (byte [] encryptedMessage, PrivateKey private){
+        String decryptedMessage = "";
         return decryptedMessage;
     }
 }
