2b49839b49fea9dbb2e2e19cb765358afc1da7f5
ckodonnell <codonnell@vassar.edu>
2021-03-15 09:43:30 -0400

    added comments

--- RSA.java
+++ RSA.java
@@ -1,3 +1,4 @@
+//Ciara O'Donnell and Adriana Buller worked together on this assignment
 import java.security.*;
 import java.util.*;
 import javax.crypto.*;
@@ -8,12 +9,13 @@ public class RSA {
         SecureRandom secureRandom = new SecureRandom();
         keyGen.initialize(1024, secureRandom);
 
-        KeyPair pair = keyGen.generateKeyPair();
+        KeyPair pair = keyGen.generateKeyPair(); //randomly generate keypair (a public and private key)
         PrivateKey privateKey = pair.getPrivate();
         PublicKey publicKey = pair.getPublic();
 
-         String originalText = "This is a secret message: RSA edition :)";
+        String originalText = "This is a secret message: RSA edition :)";
 
+        //main method needs to print out the originalText, encryptedText, and decryptedText
         System.out.println("Original Text : "+originalText);
 
         byte[] encryptedText = encrypt(originalText, publicKey);
@@ -25,22 +27,26 @@ public class RSA {
 
     }
     public static byte [] encrypt (String originalText, PublicKey pubKey) throws BadPaddingException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException{
-
+        //get Cipher instance
         Cipher encrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-
+        
+        //initialize cipher
         encrypt.init(Cipher.ENCRYPT_MODE, pubKey);
-
+        
+        //perform encryption
         return encrypt.doFinal(originalText.getBytes());
     }
     
     public static String decrypt (byte [] encryptedMessage, PrivateKey privKey) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
-
+        //get Cipher instance
         Cipher decrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 
         decrypt.init(Cipher.DECRYPT_MODE, privKey);
 
+        //perform decryption
         byte [] decryptedBytes = decrypt.doFinal(encryptedMessage);
 
+        //convert decrypted bytes into a string
         String decryptedText = new String(decryptedBytes);
         return decryptedText;
     }
